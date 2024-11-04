e0a3190377e48b227a592eae92a0c0e53dff9cab
nikyarora <nikyarora@gmail.com>
2024-11-03 19:47:38 -0500

    updates to website

--- src/About/About.js
+++ src/About/About.js
@@ -87,9 +87,8 @@ const About = () => {
             My research studies the intersection between machine learning and systems. I focus on understanding the quality and efficiency tradeoffs between alternative ML architectures using theory and empirical lenses (<a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2312.04927">Zoology</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href=" https://arxiv.org/abs/2310.12109">Monarch Mixer</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2210.02441">AMA</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2005.09117">Random Embeddings</a>). I develop new architectures and algorithms to push the Pareto frontier of these tradeoff spaces (<a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2402.18668">Based</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2407.05483">JRT</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2402.07440">M2-BERT</a>). I build systems to translate our ML insights to practical improvements (<a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2304.09433">Evaporate</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://github.com/HazyResearch/ThunderKittens">ThunderKittens</a>). 
             <br/><br/>
             
-            
-
             I recently co-created and taught a <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://cs229s.stanford.edu/fall2023/">new systems for machine learning course (CS 229s)</a> at Stanford!<br/><br/>
+            <b>I am on the academic job market 2024-25. If you think I could be a good fit for your organization, please reach out!</b>
         </div>
       </div>
     </div>
