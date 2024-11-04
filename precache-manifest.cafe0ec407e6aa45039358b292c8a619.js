f0575f3f0e2fc17be75f6eeb49fe71617b243e2d
nikyarora <nikyarora@gmail.com>
2024-11-03 19:53:04 -0500

    Updates

---
+++ precache-manifest.cafe0ec407e6aa45039358b292c8a619.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c394be5b983ccfdbf20d0890455ece",
+    "url": "/index.html"
+  },
+  {
+    "revision": "356b70d79ae59d315562",
+    "url": "/static/css/main.7a9c127a.chunk.css"
+  },
+  {
+    "revision": "59491691d97001d233c0",
+    "url": "/static/js/2.db070506.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.db070506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "356b70d79ae59d315562",
+    "url": "/static/js/main.1d381f2c.chunk.js"
+  },
+  {
+    "revision": "6d96a40039a4a2a449ec",
+    "url": "/static/js/runtime-main.0fb70ea9.js"
+  },
+  {
+    "revision": "48fea1ba65c79309c1098d5d404bd519",
+    "url": "/static/media/Metadata_Shaping.48fea1ba.pdf"
+  },
+  {
+    "revision": "5254c265bcf7af504fd912eff68de4e6",
+    "url": "/static/media/Simran_Arora_CV.5254c265.pdf"
+  },
+  {
+    "revision": "5c31f79da2bda21a452ca9cd96176da4",
+    "url": "/static/media/github-brands-solid.5c31f79d.svg"
+  },
+  {
+    "revision": "598e459f436f1e16d7c4144e49a84d6f",
+    "url": "/static/media/google-scholar-brands-solid.598e459f.svg"
+  },
+  {
+    "revision": "021a54debfdb830082e0d9696348873a",
+    "url": "/static/media/grasp_presentation.021a54de.pdf"
+  },
+  {
+    "revision": "5f3be71224b15f113710d5c360a5a48a",
+    "url": "/static/media/naval_presentation.5f3be712.pdf"
+  },
+  {
+    "revision": "89133b54ee88f0c6930ea8ace80756f1",
+    "url": "/static/media/p3p_presentation.89133b54.pdf"
+  },
+  {
+    "revision": "e2633e6af7599132046f2430728eac6b",
+    "url": "/static/media/photo.e2633e6a.jpg"
+  },
+  {
+    "revision": "3ae4619b04a7c1f649e40dc48b9f3ac6",
+    "url": "/static/media/twitter-brands-solid.3ae4619b.svg"
+  }
+]);
