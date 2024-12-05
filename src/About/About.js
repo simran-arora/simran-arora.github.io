caac1101319c08a827228683b61232d2c1f16271
Simran Arora <emailsimran@gmail.com>
2024-12-05 00:55:29 -0800

    update

--- src/About/About.js
+++ src/About/About.js
@@ -88,22 +88,27 @@ const About = () => {
             
             {/* , <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://ai.stanford.edu/">Stanford AI</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://crfm.stanford.edu/">Stanford CRFM</a> and the <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="http://statsml.stanford.edu/">Statistical Machine Learning</a> groups, and  */}
 
-            My research is in <b>AI Systems</b>. I work on developing AI that scales and uses the hardware efficiently. 
+            My research is in AI systems. I focus on expanding the Pareto frontier between quality and efficiency, to unlock new AI capabilities, by considering AI algorithms, hardware, and applications in lockstep. Recently, I've looked at:
             <br/>
             <ul className="notes-list">
             <li>
-            <b>Hardware</b>: As the complexity of AI hardware increases, how can we make it easier to extract high utilization for new AI algorithms? (<a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://github.com/HazyResearch/ThunderKittens">ThunderKittens</a>)
+            <b>Algorithms</b>: How do we build AI architectures that scale efficiently? 
+            <br/>(<a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2312.04927">Zoology</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://www.together.ai/blog/based">Based</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2407.05483">JRT</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://hazyresearch.stanford.edu/blog/2024-10-14-lolcats-p1">LoLCATS</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://www.together.ai/blog/monarch-mixer">Monarch Mixer</a>) 
             </li>
             <li>
-            <b>Software algorithms</b>: How do we build AI algorithms that scale and use the hardware efficiently? (<a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2312.04927">Zoology</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://www.together.ai/blog/based">Based</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2407.05483">JRT</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://hazyresearch.stanford.edu/blog/2024-10-14-lolcats-p1">LoLCATS</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://www.together.ai/blog/monarch-mixer">Monarch Mixer</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2005.09117">Random Embeddings</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://openreview.net/pdf?id=Qf4qZgDDj6">Machine Precision</a>) 
+            <b>Hardware</b>: As the complexity of AI hardware increases, how can we make it easier to extract high utilization for new AI algorithms? 
+            <br/>
+            (<a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://github.com/HazyResearch/ThunderKittens">ThunderKittens</a>)
             </li>
             <li>
-            <b>Applications</b>: What are the most efficient ways to use our models in AI applications? (<a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2210.02441">AMA</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2304.09433">Evaporate</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://ai.meta.com/blog/building-systems-to-reason-securely-over-private-data/">Private Search</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2403.09603/">Verifiable Training</a>)
+            <b>Applications</b>: How do we bridge the efficiency gaps that emerge as models are deployed in new settings, such as data management?
+            <br/> 
+            (<a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2210.02441">AMA</a> for <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/pdf/2205.13722">personal data</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2304.09433">Evaporate</a>, <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/2205.09911">Data Wrangling</a>)
             </li>
             </ul>
             {/* We have been excited to see the impact of this work. This work is recognized by Outstanding Paper at NeurIPS, Best Paper at ICML ES-FoMo, Oral Awards at NeurIPS, and Spotlight Awards at ICLR and ICML. On the systems side, our work is published at VLDB, SIGMOD, CIDR, CHI and is used in industry (Together AI, Snorkel AI, Apple, Jump Trading, MongoDB, Jina, LangChain, LlamaIndex). */}
             {/* <br/><br/> */}
-            I recently co-created and taught a <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://cs229s.stanford.edu/fall2023/">new systems for machine learning course (CS 229s)</a> at Stanford!<br/><br/>
+            Recently created / taught <a class="abt_a" target= "_blank" rel="noopener noreferrer" href="https://cs229s.stanford.edu/fall2023/">systems for machine learning (CS 229s)</a> at Stanford, materials are released.<br/><br/>
             <b>I am on the academic job market 2024-25!</b>
         </div>
       </div>
